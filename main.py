75f6e61868a66f0f9151d05698fbd83b5c0a266b
Tasneem <khantasneem647@gmail.com>
2026-03-02 22:16:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 # AIM: Write a Python program to calculate the gross salary of an employee.
-# Coder:
-# Date:
+# Coder:Tasneem Khan
+# Date:02-03-2026
 
 # Write your code here
+print("- Gross Salary Calculator-")
+#To get base salary from user
+bs=float(input("Enter your base salary:"))
+print("Basic salary is",bs)
+da=0.7*bs
+print("Daily allowance is ",da)
+ta=0.3*bs
+print("Travel allowance is",ta)
+hra=0.1*bs
+print("House rent allowance is",hra)
+gross_salary=bs+da+ta+hra 
+print("Gross salary:",gross_salary)
